332a3cd77aff57456577423ee91170f1e4f57dfe
rewell namu <riwenamu@gmail.com>
2025-07-29 23:00:04 +0300

    cloud transformation atlas

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/mufu_church', {
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+  useUnifiedTopology: true
+});
 
 // Routes
 const contactRoutes = require('./routes/contact');
